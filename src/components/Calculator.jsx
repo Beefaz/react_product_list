6c371829fc555f10240c9ec99676d65447e9cf43
Beefaz <justinas.geryba@gmail.com>
2024-03-02 01:28:53 +0200

    moved logic outside calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,38 +5,39 @@ import {evaluate} from "mathjs";
 
 const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 const actions = ['+', '-', '*', '/', 'C', '='];
+
+const generateButtons = (arr, state, setState) =>
+  arr.map((symbol, index) =>
+    <Button
+      key={index}
+      onClick={() => handleClick(symbol, state, setState)}>
+      {symbol === '*' ? '∗' : symbol}
+    </Button>);
+
+const handleClick = (symbol, state, setState) => {
+  if (symbol === 'C') return setState('');
+  if (state.includes('=')) return;
+
+  if (symbol === '=') try {
+    return setState(`${state} = ${evaluate(state)}`);
+  } catch {
+    return setState(`Error`);
+  }
+  if (actions.includes(symbol)) return setState(`${state} ${symbol} `);
+  return setState(`${state}${symbol}`);
+}
+
 const Calculator = () => {
   const [input, setInput] = useState('');
-
-  const handleClick = (symbol) => {
-    if (symbol === 'C') return setInput('');
-    if (input.includes('=')) return;
-
-    if (symbol === '=') try {
-      return setInput(`${input} = ${evaluate(input)}`);
-    } catch {
-      return setInput(`Error`);
-    }
-    if (actions.includes(symbol)) return setInput(`${input} ${symbol} `);
-    return setInput(`${input}${symbol}`);
-  }
-
-  const generateButtons = (arr) =>
-    arr.map((symbol, index) =>
-      <Button
-        key={index}
-        onClick={() => handleClick(symbol)}>
-        {symbol === '*' ? '∗' : symbol}
-      </Button>);
 
   return (
     <div className="calc">
       <div className="calc-controller">
         <div className="input">{input}</div>
         <div className="numbers">
-          {generateButtons(numbers)}
+          {generateButtons(numbers, input, setInput)}
         </div>
-        {generateButtons(actions)}
+        {generateButtons(actions, input, setInput)}
       </div>
     </div>
   );
